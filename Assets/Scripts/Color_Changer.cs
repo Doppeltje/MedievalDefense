aed563aa0f38d815760c5ee4162e04683744ca61
Doppeltje <Doppeltje@users.noreply.github.com>
2018-11-14 10:25:05 +0100

    first com

---
+++ Assets/Scripts/Color_Changer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color_Changer : MonoBehaviour
+{
+    public float redFade = 1;
+    public float blueFade = 1;
+    public float greenFade = 1;
+    public float opacity = 1;
+
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color fade = new Color(redFade, blueFade, greenFade, opacity);
+
+        rend.material.color = fade;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "arrow(Clone)")
+        {
+            blueFade = blueFade - .2f;
+            greenFade = greenFade - .2f;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float fireRate = 0;
+    public float Damage = 10;
+    public LayerMask whatToHit;
+
+    public Transform ArrowTrailPrefab;
+
+    private Vector2 direction;
+    private Vector2 direction2;
+
+    Transform firePoint;
+    Lock_On _lockOn;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("FirePoint not found. Check inspector.");
+        }
+        _lockOn = FindObjectOfType<Lock_On>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (fireRate == 0)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Shoot();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        float enemyX = _lockOn.targetX;
+        float enemyY = _lockOn.targetY;
+
+        //float angle;
+        float angle2;
+        //Vector3 mousePosition = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 
+        //                            Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        Vector3 enemyPosition = new Vector3(enemyX, enemyY, 0);
+        Vector3 firePointPosition = new Vector3(firePoint.position.x, firePoint.position.y, 0);
+
+        
+
+
+        enemyPosition.x = enemyPosition.x - firePointPosition.x;
+        enemyPosition.y = enemyPosition.y - firePointPosition.y;
+
+        angle2 = Mathf.Atan2(enemyPosition.y, enemyPosition.x) * Mathf.Rad2Deg;
+
+        firePoint.rotation = Quaternion.Euler(0, 0, angle2);
+
+        direction2 = enemyPosition - firePointPosition;
+
+        RaycastHit2D hit2 = Physics2D.Raycast(firePointPosition, direction2, 100, whatToHit);
+        Effect2();
+
+
+
+        if (hit2.collider != null)
+        {
+            return;
+        }
+    }
+
+
+    void Effect2()
+    {
+        Instantiate(ArrowTrailPrefab, firePoint.position, firePoint.rotation);
+        //Debug.Log("direction = " + direction2);
+    }
+}
